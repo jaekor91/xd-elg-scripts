0e7b5106d19e36038de1b0accb226cb9ce72a5e4
Jae Hyeon Lee <jaekor91@gmail.com>
2017-05-16 15:39:35 -0400

    Initial commit.

---
+++ produce-deep2-f234-photo-redz-oii.py
@@ -0,0 +1,12 @@
+# Import packages
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.io import ascii, fits
+from astropy.wcs import WCS
+import numpy.lib.recfunctions as rec
+
+# Constants
+large_random_constant = -999119283571
+deg2arcsec=3600
+
+# 
