02947a3b4e09745969bd82d75dd9cd7b3ced2506
Jae Hyeon Lee <jaekor91@gmail.com>
2017-05-18 14:49:43 -0400

    Commits of working version of XD_selection_module and apply-XD-fiducial*

--- XD_selection_module.py
+++ XD_selection_module.py
@@ -2,14 +2,16 @@ import numpy as np
 
 cnames = ["Gold", "Silver", "LowOII", "NoOII", "LowZ", "NoZ", "D2reject", "DR3unmatched","D2unobserved"]
 
-def apply_XD_globalerror(objs, last_FoM, glim=23.8, rlim=23.4, zlim=22.4, gr_ref=0.5,\
+def apply_XD_globalerror(objs, last_FoM, param_directory, glim=23.8, rlim=23.4, zlim=22.4, gr_ref=0.5,\
                        rz_ref=0.5,reg_r=1e-4/(0.025**2 * 0.05),f_i=[1., 1., 0., 0.25, 0., 0.25, 0.],\
-                       gmin = 21., gmax = 24.):
+                       gmin = 21., gmax = 24., K_i = [2,2,2,3,2,2,7], dNdm_type = [1, 1, 0, 1, 0, 0, 1]):
     """ Apply ELG XD selection. Default uses fiducial set of parameters.
 
     Args:
-        objs: A DECaLS fits table.
+        objs: A DECaLS fits table or the following list of extracted columns. 
+            objs = [g,r,z, givar, rivar, zivar, gflux, rflux, zflux]
         last_FoM: Threshold FoM.
+        param_directory: Directory where parameter files named in expected formats are to be found.
         
     Optional:
         glim, rlim, zlim: 5-sigma detection limiting magnitudes. 
@@ -18,7 +20,7 @@ def apply_XD_globalerror(objs, last_FoM, glim=23.8, rlim=23.4, zlim=22.4, gr_ref
             behaviors of the selection boundary.
         f_i: Various class weights for FoM.
         gmin, gmax: Minimum and maximum g-magnitude range to consider.
-
+        K_i and dNdm_type: See generate_XD_model_dictionary() doc string.
     
     Returns:
         iXD: Boolean mask array that implements XD selection.
@@ -43,171 +45,45 @@ def apply_XD_globalerror(objs, last_FoM, glim=23.8, rlim=23.4, zlim=22.4, gr_ref
         - For each object that passes the above two cuts, compute Figure of Merit FoM.
         - If FoM>FoM_last, then include the object in the selection.
         - Append this selection column to the table and return.
-
     """
-    ####### Density parameters hard coded in. np.float64 used for maximal precision. #######
-    params ={(0, 'mean'): np.array([[ 0.374283820390701,  1.068873405456543],
-           [ 0.283886760473251,  0.733299076557159]]),
-    (1, 'mean'): np.array([[ 0.708186626434326,  1.324055671691895],
-           [ 0.514687597751617,  0.861691951751709]]),
-    (2, 'mean'): np.array([[ 0.851126551628113,  1.49790346622467 ],
-           [ 0.593997478485107,  1.027981519699097]]),
-    (3, 'mean'): np.array([[ 0.621764063835144,  0.677076101303101],
-           [ 1.050391912460327,  1.067378640174866]]),
-    (4, 'mean'): np.array([[ 0.29889178276062 ,  0.158586874604225],
-           [ 0.265404641628265,  0.227356120944023],
-           [ 1.337790369987488,  1.670260787010193]]),
-    (5, 'mean'): np.array([[ 0.169899195432663,  0.333086401224136],
-           [ 0.465608537197113,  0.926179945468903]]),
-    (6, 'mean'): np.array([[ 0.404752403497696,  0.157505303621292],
-           [ 1.062281489372253,  0.708624482154846],
-           [ 0.767854988574982,  0.410259902477264],
-           [ 1.830820441246033,  1.096370458602905],
-           [ 1.224291563034058,  0.748376846313477],
-           [ 0.623223185539246,  0.588687479496002],
-           [ 1.454894185066223,  1.615718483924866]]),
-    (0, 'amp'): np.array([ 0.244611976587951,  0.755388023412049]),
-    (1, 'amp'): np.array([ 0.114466286005043,  0.885533713994957]),
-    (2, 'amp'): np.array([ 0.138294309756769,  0.861705690243231]),
-    (3, 'amp'): np.array([ 0.509696013263716,  0.490303986736284]),
-    (4, 'amp'): np.array([ 0.264565190839574,  0.464308147030861,  0.271126662129565]),
-    (5, 'amp'): np.array([ 0.803360982047185,  0.196639017952815]),
-    (6, 'amp'): np.array([ 0.09128923215233 ,  0.254327925723203,  0.31780750840433 ,
-            0.036144574976436,  0.145786317010496,  0.031381535653226,
-            0.12326290607998 ]),
-    (0, 'covar'): np.array([[[ 0.10418130703232 ,  0.014280057648813],
-            [ 0.014280057648813,  0.070314900027689]],
 
-           [[ 0.023818843706279,  0.018202660741959],
-            [ 0.018202660741959,  0.041376141039073]]]),
-    (1, 'covar'): np.array([[[ 0.215211984773353,  0.054615838823342],
-            [ 0.054615838823342,  0.049833562813203]],
-
-           [[ 0.04501376209018 ,  0.017654245897094],
-            [ 0.017654245897094,  0.036243604905033]]]),
-    (2, 'covar'): np.array([[[ 0.393998394239911,  0.08339271763515 ],
-            [ 0.08339271763515 ,  0.043451758548033]],
-
-           [[ 0.104132127558071,  0.066660191134385],
-            [ 0.066660191134385,  0.099474014771686]]]),
-    (3, 'covar'): np.array([[[ 0.077655250186381,  0.048031436118266],
-            [ 0.048031436118266,  0.104180325930248]],
-
-           [[ 0.18457377102254 ,  0.13405411581603 ],
-            [ 0.13405411581603 ,  0.11061389825436 ]]]),
-    (4, 'covar'): np.array([[[ 0.004346580392509,  0.002628470120243],
-            [ 0.002628470120243,  0.003971775282994]],
-
-           [[ 0.048642690792318,  0.010716631911343],
-            [ 0.010716631911343,  0.061199277021983]],
-
-           [[ 0.042759461532687,  0.038563281355028],
-            [ 0.038563281355028,  0.136138353942557]]]),
-    (5, 'covar'): np.array([[[ 0.016716270750336, -0.002912143075387],
-            [-0.002912143075387,  0.048058573349518]],
-
-           [[ 0.162280075685762,  0.056056904861885],
-            [ 0.056056904861885,  0.123029790628176]]]),
-    (6, 'covar'): np.array([[[ 0.008867550173445,  0.005830414294608],
-            [ 0.005830414294608,  0.004214767113419]],
-
-           [[ 0.128202602012536,  0.102774200195474],
-            [ 0.102774200195474,  0.103174267985407]],
-
-           [[ 0.040911683088027,  0.017665837401128],
-            [ 0.017665837401128,  0.013744306762296]],
-
-           [[ 0.007956756372728,  0.01166041211521 ],
-            [ 0.01166041211521 ,  0.030148938891721]],
-
-           [[ 0.096468861178697,  0.036857159884246],
-            [ 0.036857159884246,  0.016938035737711]],
-
-           [[ 0.112556609450265, -0.027450040449295],
-            [-0.027450040449295,  0.108044495426867]],
-
-           [[ 0.008129216729562,  0.026162239500016],
-            [ 0.026162239500016,  0.163188167512441]]]),
-    (0, 'dNdm'): np.array([  4.192577862669580e+00,   2.041560039425720e-01,
-             5.211356980204467e-01,   1.133059580454155e+03]),
-    (1, 'dNdm'): np.array([  3.969155875747644e+00,   2.460106047909254e-01,
-             7.649675390577662e-01,   1.594000900095526e+03]),
-    (2, 'dNdm'): np.array([ -2.75804468990212 ,  84.684286895340932]),
-    (3, 'dNdm'): np.array([   5.366276446077002,    0.931168472808592,    1.362372397828176,
-            159.580421075961794]),
-    (4, 'dNdm'): np.array([  -0.415601564925459,  125.965707251899474]),
-    (5, 'dNdm'): np.array([  -2.199904276713916,  206.28117629545153 ]),
-    (6, 'dNdm'): np.array([  8.188847496561811e-01,  -4.829571612433957e-01,
-             2.953829284553960e-01,   1.620279479977582e+04])
-    }
-
-    # ####### Load paramters - method 2. Rather than hardcoding in the parameters,
-    # we could also import them from a file ######
-    # def generate_XD_model_dictionary(tag1="glim24", tag2="", K_i = [2,2,2,2,3,2,7], dNdm_type = [1, 1, 0, 1, 0, 0, 1]):
-    #     # Create empty dictionary
-    #     params = {}
-        
-    #     # Adding dNdm parameters for each class
-    #     for i in range(7):
-    #         if dNdm_type[i] == 0:
-    #             dNdm_params =np.loadtxt(("%d-fit-pow-"+tag1)%i)
-    #         else:
-    #             dNdm_params =np.loadtxt(("%d-fit-broken-"+tag1)%i)
-    #         params[(i, "dNdm")] = dNdm_params
-            
-    #     # Adding GMM parameters for each class
-    #     for i in range(7):
-    #         amp, mean, covar = load_params_XD(i,K_i[i],tag0="fit",tag1=tag1,tag2=tag2)
-    #         params[(i,"amp")] = amp
-    #         params[(i,"mean")] = mean
-    #         params[(i,"covar")] = covar
-            
-    #     return params
-
-    # def load_params_XD(i,K,tag0="fit",tag1="glim24",tag2=""):
-    #     fname = ("%d-params-"+tag0+"-amps-"+tag1+"-K%d"+tag2+".npy") %(i, K)
-    #     amp = np.load(fname)
-    #     fname = ("%d-params-"+tag0+"-means-"+tag1+"-K%d"+tag2+".npy") %(i, K)
-    #     mean= np.load(fname)
-    #     fname = ("%d-params-"+tag0+"-covars-"+tag1+"-K%d"+tag2+".npy") %(i, K)
-    #     covar  = np.load(fname)
-    #     return amp, mean, covar        
-
-    # params = generate_XD_model_dictionary()
-
-
+    params = generate_XD_model_dictionary(param_directory, K_i=K_i, dNdm_type=dNdm_type)
 
     ####### Load variables. #######
-    # Flux
-    gflux = objs['DECAM_FLUX'][:][:,1]/objs['DECAM_MW_TRANSMISSION'][:][:,1] 
-    rflux = objs['DECAM_FLUX'][:][:,2]/objs['DECAM_MW_TRANSMISSION'][:][:,2]
-    zflux = objs['DECAM_FLUX'][:][:,4]/objs['DECAM_MW_TRANSMISSION'][:][:,4]
-    # mags
-    #ADM added explicit capture of runtime warnings for zero and negative fluxes
-    with np.errstate(invalid='ignore',divide='ignore'):
-        g = (22.5 - 2.5*np.log10(gflux)) 
-        r = (22.5 - 2.5*np.log10(rflux))
-        z = (22.5 - 2.5*np.log10(zflux))
-        # Inver variance
-        givar = objs['DECAM_FLUX_IVAR'][:][:,1]
-        rivar = objs['DECAM_FLUX_IVAR'][:][:,2]
-        zivar = objs['DECAM_FLUX_IVAR'][:][:,4]
-        # Color
-        rz = (r-z); gr = (g-r)    
-    
-        ####### Reaonsable quaity cut. #######
-        iflux_positive = (gflux>0)&(rflux>0)&(zflux>0)
-        ireasonable_color = (gr>-0.5) & (gr<2.5) & (rz>-0.5) &(rz<2.7) & (g<gmax) & (g>gmin)
-        thres = 2
-        igrz_SN2 =  ((gflux*np.sqrt(givar))>thres)&((rflux*np.sqrt(rivar))>thres)&((zflux*np.sqrt(zivar))>thres)
-        # Combination of above cuts.
-        ireasonable = iflux_positive & ireasonable_color & igrz_SN2
-    
-        ####### A rough cut #######
-        irough = (gr<1.3) & np.logical_or(gr<(rz+0.3) ,gr<0.3)
+    if type(objs) is list:
+        g,r,z, givar, rivar, zivar, gflux, rflux, zflux = objs        
+    else:
+        # Flux
+        gflux = objs['DECAM_FLUX'][:][:,1]/objs['DECAM_MW_TRANSMISSION'][:][:,1] 
+        rflux = objs['DECAM_FLUX'][:][:,2]/objs['DECAM_MW_TRANSMISSION'][:][:,2]
+        zflux = objs['DECAM_FLUX'][:][:,4]/objs['DECAM_MW_TRANSMISSION'][:][:,4]
+        # mags
+        #ADM added explicit capture of runtime warnings for zero and negative fluxes
+        with np.errstate(invalid='ignore',divide='ignore'):
+            g = (22.5 - 2.5*np.log10(gflux)) 
+            r = (22.5 - 2.5*np.log10(rflux))
+            z = (22.5 - 2.5*np.log10(zflux))
+            # Inver variance
+            givar = objs['DECAM_FLUX_IVAR'][:][:,1]
+            rivar = objs['DECAM_FLUX_IVAR'][:][:,2]
+            zivar = objs['DECAM_FLUX_IVAR'][:][:,4]
 
-        ####### Objects for which FoM to be calculated. #######
-        ibool = ireasonable & irough 
+    # Color
+    rz = (r-z); gr = (g-r)    
+
+    ####### Reaonsable quaity cut. #######
+    iflux_positive = (gflux>0)&(rflux>0)&(zflux>0)
+    ireasonable_color = (gr>-0.5) & (gr<2.5) & (rz>-0.5) &(rz<2.7) & (g<gmax) & (g>gmin)
+    thres = 2
+    igrz_SN2 =  ((gflux*np.sqrt(givar))>thres)&((rflux*np.sqrt(rivar))>thres)&((zflux*np.sqrt(zivar))>thres)
+    # Combination of above cuts.
+    ireasonable = iflux_positive & ireasonable_color & igrz_SN2
+
+    ####### A rough cut #######
+    irough = (gr<1.3) & np.logical_or(gr<(rz+0.3) ,gr<0.3)
+
+    ####### Objects for which FoM to be calculated. #######
+    ibool = ireasonable & irough 
         
     
     ######## Compute FoM values for objects that pass the cuts. #######
@@ -230,7 +106,11 @@ def apply_XD_globalerror(objs, last_FoM, glim=23.8, rlim=23.4, zlim=22.4, gr_ref
     # Helper function 1.
     def GMM_vectorized(gr, rz, amps, means, covars, gvar, rvar, zvar):
         """
-        Color-color density    
+        Color-color density.
+
+        Note 1: This routine was originally written based on gr vs. rz convention. However, I decided to adopt
+        rz vs. gr convention and therefore, the user must provide "gr=rz, rz=gr" as input in order for this
+        function to work properly.
 
         Params
         ------
@@ -287,7 +167,7 @@ def apply_XD_globalerror(objs, last_FoM, glim=23.8, rlim=23.4, zlim=22.4, gr_ref
     FoM_num = np.zeros_like(gr)
     FoM_denom = np.zeros_like(gr)
     for i in range(7): # number of classes.
-        n_i = GMM_vectorized(gr,rz, params[i, "amp"], params[i, "mean"],params[i, "covar"], gvar, rvar, zvar)  * dNdm(params[(i,"dNdm")], flux)
+        n_i = GMM_vectorized(rz, gr, params[i, "amp"], params[i, "mean"],params[i, "covar"], gvar, rvar, zvar)  * dNdm(params[(i,"dNdm")], flux)
         FoM_num += f_i[i]*n_i
         FoM_denom += n_i
            
@@ -299,3 +179,41 @@ def apply_XD_globalerror(objs, last_FoM, glim=23.8, rlim=23.4, zlim=22.4, gr_ref
     return iXD, FoM
 
 
+def generate_XD_model_dictionary(param_directory, tag1="glim24", tag2="", K_i = [2,2,2,2,3,2,7], dNdm_type = [1, 1, 0, 1, 0, 0, 1]):
+    """
+    Construct a Python dictionary of model parameters for all the relevant classes.
+    
+    param_directory: directory where the files with expected formats are to be found.
+    tag1, tag2: are used to name the files.
+    K_i: Number of components for each class of objects.
+    dNDm_type: Type of function to use to model g-mag distribution. 
+        0 corresponds to power law and 1 to broken power law.
+    """
+    # Create empty dictionary
+    params = {}
+    
+    # Adding dNdm parameters for each class
+    for i in range(7):
+        if dNdm_type[i] == 0:
+            dNdm_params =np.loadtxt((param_directory+"%d-fit-pow-"+tag1)%i)
+        else:
+            dNdm_params =np.loadtxt((param_directory+"%d-fit-broken-"+tag1)%i)
+        params[(i, "dNdm")] = dNdm_params
+        
+    # Adding GMM parameters for each class
+    for i in range(7):
+        amp, mean, covar = load_params_XD(param_directory,i,K_i[i],tag0="fit",tag1=tag1,tag2=tag2)
+        params[(i,"amp")] = amp
+        params[(i,"mean")] = mean
+        params[(i,"covar")] = covar
+        
+    return params
+
+def load_params_XD(param_directory,i,K,tag0="fit",tag1="glim24",tag2=""):
+    fname = (param_directory+"%d-params-"+tag0+"-amps-"+tag1+"-K%d"+tag2+".npy") %(i, K)
+    amp = np.load(fname)
+    fname = (param_directory+"%d-params-"+tag0+"-means-"+tag1+"-K%d"+tag2+".npy") %(i, K)
+    mean= np.load(fname)
+    fname = (param_directory+"%d-params-"+tag0+"-covars-"+tag1+"-K%d"+tag2+".npy") %(i, K)
+    covar  = np.load(fname)
+    return amp, mean, covar        
---
+++ apply-XD-fiducial-DEEP2.py
@@ -0,0 +1,109 @@
+# Load modules
+import numpy as np
+from xd_elg_utils import *
+# Note that table_utils must be called after xd_elg_utils to overwrite certain
+# routine definitions. I understand that this is a bad practice but for this
+# non-critical application, I will condone this.
+from table_utils import *
+import XD_selection_module as XD
+
+
+# Constants
+large_random_constant = -999119283571
+deg2arcsec=3600
+colors = ["orange", "grey", "brown", "purple", "red", "salmon","black", "white","blue"]
+cnames = ["Gold", "Silver", "LowOII", "NoOII", "LowZ", "NoZ", "D2reject", "DR3unmatched","D2unobserved"]
+
+##############################################################################
+print("1. Load DR3-DEEP2 data.")
+# Field 2
+set2 = load_fits_table("DECaLS-DR3-DEEP2f2-glim24.fits")
+set2 = set2[reasonable_mask(set2)] # Applying reasonable mask
+grz2 = load_grz(set2)
+cn2 = load_cn(set2)
+w2 = load_weight(set2)
+grzflux2 = load_grz_flux(set2)
+grzivar2 = load_grz_invar(set2)
+d2m2 = load_DEEP2matched(set2) # DEEP2_matched?
+redz2 = load_redz(set2)
+
+# Field 3
+set3 = load_fits_table("DECaLS-DR3-DEEP2f3-glim24.fits")
+set3 = set3[reasonable_mask(set3)] # Applying reasonable mask
+grz3 = load_grz(set3)
+cn3 = load_cn(set3)
+w3 = load_weight(set3)
+grzflux3 = load_grz_flux(set3)
+grzivar3 = load_grz_invar(set3)
+d2m3 = load_DEEP2matched(set3) # DEEP2_matched? 
+redz3 = load_redz(set3)
+
+# Field 4
+set4 = load_fits_table("DECaLS-DR3-DEEP2f4-glim24.fits")
+set4 = set4[reasonable_mask(set4)] # Applying reasonable mask
+grz4 = load_grz(set4)
+cn4 = load_cn(set4)
+w4 = load_weight(set4)
+grzflux4 = load_grz_flux(set4)
+grzivar4 = load_grz_invar(set4)
+d2m4 = load_DEEP2matched(set4) # DEEP2_matched? 
+redz4 = load_redz(set4)
+
+# Load the intersection area
+area = np.loadtxt("intersection-area-f234").sum()
+
+# Combine 
+cn = np.concatenate((cn2,cn3,cn4))
+w = np.concatenate((w2, w3, w4))
+grz = combine_grz(grz2, grz3, grz4)
+grzflux = combine_grz(grzflux2, grzflux3, grzflux4)
+grzivar = combine_grz(grzivar2, grzivar3, grzivar4)
+d2m = np.concatenate((d2m2, d2m3, d2m4))
+num_unmatched = (d2m==0).sum()
+redz = np.concatenate((redz2, redz3, redz4))
+# print("Total number of unmatched objects: %d" % num_unmatched)
+# print("In density: %.2f" % (num_unmatched/area.sum()))
+# print((cn<0).sum())
+
+# Giving unmatched objects its proper number.
+cn[cn<0] = 7
+print("Completed.\n")
+
+
+##############################################################################
+print("2. Impose XD cut.")
+last_FoM = 0.502
+param_directory = "./" # Directory where the parameters are saved.
+# Unpack variables
+g,r,z = grz
+givar, rivar, zivar = grzivar
+gflux, rflux, zflux = grzflux
+
+iXD, FoM = XD.apply_XD_globalerror([g, r, z, givar, rivar, zivar, gflux, rflux, zflux], last_FoM, param_directory, glim=23.8, rlim=23.4, zlim=22.4, gr_ref=0.5,\
+                       rz_ref=0.5,reg_r=1e-4/(0.025**2 * 0.05),f_i=[1., 1., 0., 0.25, 0., 0.25, 0.],\
+                       gmin = 21., gmax = 24., K_i = [2,2,2,3,2,2,7], dNdm_type = [1, 1, 0, 1, 0, 0, 1])
+print("Completed.\n")
+
+
+##############################################################################
+print("3. Print FDR cut result.")
+print(" & ".join(["Cut", "Type", "Gold", "Silver", "LowOII", "NoOII", "LowZ", "NoZ", "D2reject", "DR3unmatched", \
+      "DESI", "Total", "Eff", "FoM"]) + "\\\\ \hline") 
+return_format = ["FDR", "Avg.", "Gold", "Silver", "LowOII", "NoOII", "LowZ", "NoZ", "D2reject", "DR3unmatched", \
+      "DESI", "Total", "Eff", str("%.3f"%last_FoM),  "\\\\ \hline"]
+print(class_breakdown_cut(cn[iXD], w[iXD], area,rwd="D", num_classes=8, \
+     return_format = return_format,\
+     class_eff = [1., 1., 0.25, 0.25, 0., 0., 0., 0.]
+     ))
+print("Completed.\n")
+
+
+##############################################################################
+print("4. Plot n(z) for the selection.")
+dz = 0.05
+fname = "dNdz-XD-fiducial-DEEP2-Total.png"
+plot_dNdz_selection(cn, w, iXD, redz, area, dz=0.05, gold_eff=1, silver_eff=1, NoZ_eff=0.25, NoOII_eff=0.25,\
+	iselect2=None, plot_total=True, fname=fname, color1="black", color2="red", color_total="green",\
+	label1="FDR", label2="", label_total="DEEP2 Total")
+
+print("Completed.\n")
