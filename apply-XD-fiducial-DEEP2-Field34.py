9d11be98523b45c3b9848549ae152f073309ea3c
Jae Hyeon Lee <jaekor91@gmail.com>
2017-06-08 13:44:15 -0400

    Compare Field 34 cut and Field 234 cut.

--- apply-XD-fiducial-DEEP2-Field34.py
+++ apply-XD-fiducial-DEEP2-Field34.py
@@ -106,7 +106,7 @@ print("Completed.\n")
 
 
 ##############################################################################
-print("3. Print FDR cut, XD cut, XD proj. results.")
+print("5. Print FDR cut, XD cut, XD proj. results.")
 print(" & ".join(["Cut", "Type", "Gold", "Silver", "LowOII", "NoOII", "LowZ", "NoZ", "D2reject", "DR3unmatched", \
       "DESI", "Total", "Eff", "FoM"]) + "\\\\ \hline")
 
@@ -131,7 +131,7 @@ print("Completed.\n")
 
 
 ##############################################################################
-print("4. Plot n(z) for the selection.")
+print("6. Plot n(z) for the selection.")
 dz = 0.05
 fname = "dNdz-XD-fiducial-FDR-DEEP2-Field34.png"
 plot_dNdz_selection(cn, w, iXD, redz, area, dz=0.05, \
@@ -142,27 +142,83 @@ print("Completed.\n")
 
 
 # ##############################################################################
-print("5. Create many slices for a movie/stills.")
-bnd_fig_directory = "./bnd_fig_directory/XD-fiducial-Field34/"
-fname = "XD-fiducial-f34"
+print("7. Create many slices for a movie/stills.")
+# bnd_fig_directory = "./bnd_fig_directory/XD-fiducial-Field34/"
+# fname = "XD-fiducial-f34"
 
-print("5a. Creating stills")
-for m in [22., 22.5, 23.0, 23.5, 23.75, 23.825]:
-	print("Slice %.3f"%m)
-	XD.plot_slice(grid, m, bnd_fig_directory, fname)
+# print("7a. Creating stills")
+# for m in [22., 22.5, 23.0, 23.5, 23.75, 23.825]:
+# 	print("Slice %.3f"%m)
+# 	XD.plot_slice(grid, m, bnd_fig_directory, fname)
+# print("Completed.\n")
+
+# print("7b. Creating a movie")
+# dm = w_mag
+# for i,m in enumerate(np.arange(21.5,24+0.9*w_mag, w_mag)):
+# 	print("Index %d, Slice %.3f" % (i,m))
+# 	XD.plot_slice(grid, m, bnd_fig_directory, fname, movie_tag=i)	
+
+# print("Completed.\n")
+
+# print("Command for creating a movie.:\n \
+# 	ffmpeg -r 6 -start_number 0 -i XD-fiducial-f34-mag0-%d.png -vcodec mpeg4 -y XD-fiducial-f34-movie.mp4")
+
+
+
+##############################################################################
+print("8. To compare, compute XD projection based on fiducial set of parameters.")
+param_directory = "./"
+f_i = [1., 1., 0., 0.25, 0., 0.25, 0.]
+
+start = time.time()
+grid_fiducial, last_FoM_fiducial = XD.generate_XD_selection(param_directory, glim=23.8, rlim=23.4, zlim=22.4, \
+                          gr_ref=0.5, rz_ref=0.5, N_tot=2400, f_i=f_i, \
+                          reg_r=5e-4,zaxis="g", w_cc = w_cc, w_mag = w_mag, minmag = 21.5+w_mag/2., \
+                          maxmag = 24., K_i = [2,2,2,3,2,2,7], dNdm_type = [1, 1, 0, 1, 0, 0, 1])
+print("Time taken: %.2f seconds" % (time.time()-start))
+print("Computed last FoM based on the grid: %.3f"%last_FoM)
+
+iXD_fiducial, FoM_fiducial = XD.apply_XD_globalerror([g, r, z, givar, rivar, zivar, gflux, rflux, zflux], last_FoM_fiducial, param_directory, \
+            glim=23.8, rlim=23.4, zlim=22.4, gr_ref=0.5,\
+                       rz_ref=0.5, reg_r=5e-4/(w_cc**2 * w_mag), f_i=f_i,\
+                       gmin = 21., gmax = 24., K_i = [2,2,2,3,2,2,7], dNdm_type = [1, 1, 0, 1, 0, 0, 1])
 print("Completed.\n")
 
-print("5b. Creating a movie")
+
+##############################################################################
+print("9. Create many slices for a movie/stills.")
+bnd_fig_directory = "./bnd_fig_directory/XD-fiducial-Field34-Field234-comparison/"
+fname = "XD-fiducial-Field34-Field234-comparison"
+
+print("9a. Creating stills")
+for m in [22., 22.5, 23.0, 23.5, 23.75, 23.825]:
+    print("Slice %.3f"%m)
+    XD.plot_slice_compare(grid, grid_fiducial, m, bnd_fig_directory, fname)
+print("Completed.\n")
+
+print("9b. Creating a movie")
 dm = w_mag
 for i,m in enumerate(np.arange(21.5,24+0.9*w_mag, w_mag)):
-	print("Index %d, Slice %.3f" % (i,m))
-	XD.plot_slice(grid, m, bnd_fig_directory, fname, movie_tag=i)	
+    print("Index %d, Slice %.3f" % (i,m))
+    XD.plot_slice_compare(grid, grid_fiducial, m, bnd_fig_directory, fname, movie_tag=i)   
 
 print("Completed.\n")
 
 print("Command for creating a movie.:\n \
-	ffmpeg -r 6 -start_number 0 -i XD-fiducial-f34-mag0-%d.png -vcodec mpeg4 -y XD-fiducial-f34-movie.mp4")
+    ffmpeg -r 6 -start_number 0 -i XD-fiducial-Field34-Field234-comparison-mag0-%d.png -vcodec mpeg4 -y XD-fiducial-Field34-Field234-comparison-movie.mp4")
 
 
+##############################################################################
+print("10. Make dNdm plots for both grids.")
+# Total 
+fname = "dNdm-XD-fiducial-Field34-Field234-dNdm-Total"
+XD.plot_dNdm_XD(grid, grid_fiducial, fname=fname, plot_type="Total", label1 ="Field34", \
+  class_eff =  [1., 1., 0.0, 0.6, 0., 0.25, 0.])
+
+# DESI
+fname = "dNdm-XD-fiducial-Field34-Field234-dNdm-DESI"
+XD.plot_dNdm_XD(grid, grid_fiducial, fname=fname, plot_type="DESI", label1 ="Field34", \
+  class_eff =  [1., 1., 0.0, 0.6, 0., 0.25, 0.])
+print("Completed.\n")
 
 
