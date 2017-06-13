3336858dac3d4d2bbf911665848af736dabad15a
Jae Hyeon Lee <jaekor91@gmail.com>
2017-06-13 16:24:09 -0400

    Added a plot for combined 3 and 4

--- plot-mag-depth.py
+++ plot-mag-depth.py
@@ -59,7 +59,7 @@ print("Completed.\n")
 print("2. Make depth plots based on r_exp [0.35, 0.55] objects. The objects \n\
 must possess positive flux invar and positive flux.")
 ft_size = 20
-tick_size = 15
+tick_size = 18
 lw =2 
 
 dm=0.05
@@ -97,5 +97,43 @@ for i in range(3):
     plt.savefig("mag-depth-field%d.png"%(i+2),bbox_inches="tight",dpi=400)
     # plt.show()
     plt.close()
+print("Completed.\n")
+
+
+
+##############################################################################  
+print("3. Same as above but combining Field 3 and 4.")
+r_exp = np.concatenate((r_exp3, r_exp4))
+gf, rf, zf = combine_grz(grzflux3, grzflux4)
+gi, ri, zi = combine_grz(grzivar3, grzivar4)
+gf_err, rf_err, zf_err = grz_flux_error([gi, ri, zi])
+
+ibool = (r_exp>r_exp_min)&(r_exp<r_exp_max)
+
+fig = plt.figure(figsize=(12,7))
+
+g_depth = mag_depth_Xsigma(gf_err[ibool])
+r_depth = mag_depth_Xsigma(rf_err[ibool])
+z_depth = mag_depth_Xsigma(zf_err[ibool])
+
+plt.hist(g_depth,bins=bins,histtype ="stepfilled",color="green", lw=lw,alpha=0.25)
+plt.hist(r_depth,bins=bins,histtype ="stepfilled",color="red", lw=lw,alpha=0.25)
+plt.hist(z_depth,bins=bins,histtype ="stepfilled",color="purple", lw=lw,alpha=0.25)
+plt.xlabel("mag depth", fontsize=ft_size)
+plt.ylabel("dN/d(%.2fm)"%dm, fontsize=ft_size)
+# Required
+plt.axvline(x=24.0, c="green",lw=lw,ls="--")
+plt.axvline(x=23.4, c="red",lw=lw,ls="--")
+plt.axvline(x=22.5, c="purple",lw=lw,ls="--")
+# Median
+plt.axvline(x=np.median(g_depth), c="green",lw=lw)
+plt.axvline(x=np.median(r_depth), c="red",lw=lw)
+plt.axvline(x=np.median(z_depth), c="purple",lw=lw)
+
+plt.title("Field 34", fontsize=ft_size)
+plt.tick_params(axis='both', which='major', labelsize=tick_size)    
+plt.savefig("mag-depth-field34.png",bbox_inches="tight",dpi=400)
+# plt.show()
+plt.close()
 print("Completed.\n")
     
